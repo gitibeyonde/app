7af47ad80bbd820988aeae69a44b10a07c0871f8
Abhinandan Prateek <agneya2001@ibeyonde.com>
2021-09-28 12:50:55 +0000

    fix username field type

--- login.php
+++ login.php
@@ -7,58 +7,58 @@ include (__ROOT__ . '/config/config.php');
 <script src="js/login.js"></script>
 <div class="container top">
   <section class="container-fluid h-100 my-login-page">
-	<div class="container h-100">
-		<div class="row justify-content-md-center h-100">
-			<div class="card-wrapper">
-				<div class="brand">
-					<img src="/img/ibeyonde.jpg"" alt="pagemight html builder login page">
-				</div>
-				<div class="card fat">
-					<div class="card-body">
-						<h4 class="card-title">Login</h4>
-                      	<form role="form" method="post" class="my-login-validation" novalidate="" action="/index.php?<?php echo $_SERVER['QUERY_STRING'] == "view=logout_view" ? "" : $_SERVER['QUERY_STRING']; ?>" name="loginform">
-							<div class="form-group">
-								<label for="username">Username</label>
-                                <input id="username" type="email" name="user_name" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" required autofocus>
-								<div class="invalid-feedback">
-									Username is invalid
-								</div>
-							</div>
+    <div class="container h-100">
+        <div class="row justify-content-md-center h-100">
+            <div class="card-wrapper">
+                <div class="brand">
+                    <img src="/img/ibeyonde.jpg"" alt="pagemight html builder login page">
+                </div>
+                <div class="card fat">
+                    <div class="card-body">
+                        <h4 class="card-title">Login</h4>
+                          <form role="form" method="post" class="my-login-validation" novalidate="" action="/index.php?<?php echo $_SERVER['QUERY_STRING'] == "view=logout_view" ? "" : $_SERVER['QUERY_STRING']; ?>" name="loginform">
+                            <div class="form-group">
+                                <label for="username">Username</label>
+                                <input id="username" type="username" name="user_name" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" required autofocus>
+                                <div class="invalid-feedback">
+                                    Username is invalid
+                                </div>
+                            </div>
 
-							<div class="form-group">
-								<label for="password">Password
-									<a href="/password_reset.php" class="float-right">
-										Forgot Password?
-									</a>
-								</label>
+                            <div class="form-group">
+                                <label for="password">Password
+                                    <a href="/password_reset.php" class="float-right">
+                                        Forgot Password?
+                                    </a>
+                                </label>
                                <input type="password" name="user_password" class="form-control" placeholder="Password" aria-label="Recipient's username"
-                               			aria-describedby="basic-addon2" required data-eye="" autocomplete>
-							    <div class="invalid-feedback">
-							    	Password is required
-						    	</div>
-							</div>
+                                           aria-describedby="basic-addon2" required data-eye="" autocomplete>
+                                <div class="invalid-feedback">
+                                    Password is required
+                                </div>
+                            </div>
 
-							<div class="form-group">
-								<div class="custom-checkbox custom-control">
-									<input type="checkbox" name="remember" id="remember" class="custom-control-input">
-									<label for="remember" class="custom-control-label">Remember Me</label>
-								</div>
-							</div>
+                            <div class="form-group">
+                                <div class="custom-checkbox custom-control">
+                                    <input type="checkbox" name="remember" id="remember" class="custom-control-input">
+                                    <label for="remember" class="custom-control-label">Remember Me</label>
+                                </div>
+                            </div>
 
-							<div class="form-group m-0">
+                            <div class="form-group m-0">
                                 <button type="submit" name="login" value="login" class="btn btn-warning btn-block">
-									Login
-								</button>
-							</div>
-							<div class="mt-4 text-center">
-								Don't have an account? <a href="/register.php">Create One</a>
-							</div>
-						</form>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+                                    Login
+                                </button>
+                            </div>
+                            <div class="mt-4 text-center">
+                                Don't have an account? <a href="/register.php">Create One</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 </div>
 <?php
